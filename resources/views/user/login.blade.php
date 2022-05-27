d81806dd0020d33a88b9791c97ce4be50da91f56
midoon <harunkoding@gmail.com>
2022-05-27 23:20:37 +0700

    styling login page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,33 +1,47 @@
 @extends('layout.master')
 
+@section('css')
+<link rel="stylesheet" href="css/login.css">
+@endsection
+
 @section('content')
-<div class="row">
-    <div class="col-lg-6">
-        @if(isset($error))
-        <div class="row">
-            <div class="alert alert-danger" role="alert">
-               {{$error}}
-            </div>
-        </div>
-         @endif
+
+<div class="global-container">
     
-         <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="/login">
-            <!-- <input type="hidden" name="_token" value="{{csrf_token()}}"> -->
-            @csrf
-            <div class="form-floating mb-3">
-                <input name="user" type="text" class="form-control" id="user" placeholder="id">
-                <label for="user">User</label>
+     <div class="card login-form ">
+        
+         <div class="card-body">
+            @if(isset($error))
+            <div class="row">
+                <div class="alert alert-danger" role="alert">
+                   {{$error}}
+                </div>
             </div>
-            <div class="form-floating mb-3">
-                <input name="password" type="password" class="form-control" id="password" placeholder="password">
-                <label for="password">Password</label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign In RT/RW</button>
-        </form>
-        <form action="/warga-login" class="p-4 p-md-5 border rounded-3 bg-light" method="post">
-            @csrf
-            <button class="w-100 btn btn-lg btn-primary" type="submit">MASUK SEBAGAI WARGA</button>
-        </form>
+             @endif
+             <h1 class="card-title text-center">Login</h1>
+             <div class="card-text">
+                <form class="mt-2    " method="post" action="/login">
+                    <!-- <input type="hidden" name="_token" value="{{csrf_token()}}"> -->
+                    @csrf
+                    <div class="form-floating mb-3">
+                        <input name="user" type="text" class="form-control" id="user" placeholder="id">
+                        <label for="user">User</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input name="password" type="password" class="form-control" id="password" placeholder="password">
+                        <label for="password">Password</label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary btn-block mt-1" type="submit">Masuk Sebagai RT/RW</button>
+                </form>
+                <form action="/warga-login" class="mt-2 " method="post">
+                    @csrf
+                    <label for="">Akses Warga</label>
+                    <button class="w-100 btn btn-lg btn-primary btn-block mt-2" type="submit">Masuk Sebagai Warga</button>
+                </form>
+             </div>
+         </div>
+        
+        
     </div>
 </div>
 @endsection
